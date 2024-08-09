26f1190a83f1375279abc6c51da7e38cb65012a6
jordanobl <jordan.williams@codemith.io>
2024-08-08 23:28:52 -0400

    update with onl ynedded server files and directories

--- src/helpers/Timer/nextHundrethMileSeconds.ts
+++
@@ -1,4 +0,0 @@
-function nextHundredthMileSeconds(pace: number): number {
-  return Number(((0.01 / pace) * 3600).toFixed());
-}
-export default nextHundredthMileSeconds;
--- src/helpers/capitalizeWord.ts
+++
@@ -1,6 +0,0 @@
-const CapitalizeWord = (str: string): string => {
-  const capital = str[0].toUpperCase();
-  return capital + str.slice(1, str.length);
-};
-
-export default CapitalizeWord
