5787d4554a4c782bee72e7f612ad7697b5217216
jordanobl <jordan.williams@codemith.io>
2024-08-09 13:59:46 -0400

    change updateonduplicate, triggers maybe working after reconnect

--- src/index.js
+++ src/index.js
@@ -2636,8 +2636,7 @@ app.post('/push', async (req, res) => {
     console.log('sending changes to pg', {changes, lastPulledAt});
     if (lastPulledAt !== 'null') {
       if (changes?.users?.created[0] !== undefined) {
-        const users = await User.bulkCreate(changes.users.created, { updateOnDuplicate: [ 'updated_at']},
-);
+        const users = await User.bulkCreate(changes.users.created);
       }
       if (changes?.users_achievements?.created[0] !== undefined) {
         const users_achievements = await User_Achievement.bulkCreate(
