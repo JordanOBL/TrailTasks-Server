74d97f145b08a15c875d3e9b47dc93b794a21127
jordanobl <jordan.williams@codemith.io>
2024-08-08 23:58:57 -0400

    update trail list syntax error

--- src/trailList.js
+++ src/trailList.js
@@ -140,7 +140,7 @@
       trail_difficulty: 'moderate',
       park_id: '2',
       trail_image_url:
-        https://images.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNTYyMzI3MTcvY2ZkMzZhMDQ4Y2QzNmZiMjU4YWQzMzQ4ZmMyZWNkZmQuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==',
+       'https://images.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNTYyMzI3MTcvY2ZkMzZhMDQ4Y2QzNmZiMjU4YWQzMzQ4ZmMyZWNkZmQuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJqcGVnIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ==',
       trail_elevation: '1735',
       all_trails_url:'https://www.alltrails.com/trail/american-samoa/tutuila/mount-alava-trail?u=i',
       nps_url: 'https://www.nps.gov/places/mount-alava.htm',
