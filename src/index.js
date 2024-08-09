0a443db57904887ba6a7ad8c9872d3dbd6691439
jordanobl <jordan.williams@codemith.io>
2024-08-09 13:47:04 -0400

    try updaeondupllicate for sequelize

--- src/index.js
+++ src/index.js
@@ -2636,7 +2636,8 @@ app.post('/push', async (req, res) => {
     console.log('sending changes to pg', {changes, lastPulledAt});
     if (lastPulledAt !== 'null') {
       if (changes?.users?.created[0] !== undefined) {
-        const users = await User.bulkCreate(changes.users.created);
+        const users = await User.bulkCreate(changes.users.created, { updateOnDuplicate: [ 'updated_at']},
+);
       }
       if (changes?.users_achievements?.created[0] !== undefined) {
         const users_achievements = await User_Achievement.bulkCreate(
